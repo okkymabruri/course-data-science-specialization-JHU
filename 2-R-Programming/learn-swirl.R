f3b7b0671927d1f63db4218ea9e98e9014b5fc03
okkymabruri <okkymabrur@gmail.com>
2020-09-09 18:44:21 +0700

    rename file & fix typo

--- 2-R-Programming/learn-swirl.R
+++ 2-R-Programming/learn-swirl.R
@@ -2,4 +2,4 @@ install.packages("swirl") #install swirl
 packageVersion("swirl") #check package version
 library(swirl) #import swirl package 
 install_from_swirl("R Programming") #install R programming course
-swirl() #stat swirl
+swirl() #start swirl
