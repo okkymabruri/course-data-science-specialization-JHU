d1ae009389192fb706081a19c66792611d39bbea
okkymabruri <okkymabrur@gmail.com>
2020-09-13 15:14:12 +0700

    finishing program

--- 2-R-Programming/w3-assigment-answer.R
+++ 2-R-Programming/w3-assigment-answer.R
@@ -1,1 +1,42 @@
-# Caching the Mean of a Vector
+# Assignment: Caching the Inverse of a Matrix
+
+# Matrix inversion is usually a costly computation and there may be some benefit to caching the inverse of a matrix rather than compute it repeatedly (there are also alternatives to matrix inversion that we will not discuss here). Your assignment is to write a pair of functions that cache the inverse of a matrix.
+
+makeCacheMatrix <- function(x = matrix()) {
+    i <- NULL
+    set <- function(y) {
+        x <<- y
+        i <<- NULL
+    }
+    get <- function()
+        x
+    setinverse <- function(inverse)
+        i <<- inverse
+    getinverse <- function()
+        i
+    list(
+        set = set,
+        get = get,
+        setinverse = setinverse,
+        getinverse = getinverse
+    )
+}
+
+
+cacheSolve <- function(x, ...) {
+    i <- x$getinverse()
+    if (!is.null(i)) {
+        message("getting cached data")
+        return(i)
+    }
+    data <- x$get()
+    i <- solve(data, ...)
+    x$setinverse(i)
+    i
+}
+
+
+x <- matrix(c(8, 2, 3, 4, 21, 15, 89, 7, 69), 3, 3)
+
+y <- makeCacheMatrix(x)
+cacheSolve(y)
