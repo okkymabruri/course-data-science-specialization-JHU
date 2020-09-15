00a89aa1918d8f5e2fd840728a44eced39156a52
okkymabruri <okkymabrur@gmail.com>
2020-09-15 18:30:38 +0700

    add learn swirt getting data

---
+++ 3-Getting-and-Cleaning-Data/learn-swirl-cleaningdata.R
@@ -0,0 +1,5 @@
+install.packages("swirl") #install swirl
+packageVersion("swirl") #check package version
+library(swirl) #import swirl package
+install_from_swirl("R Programming") #install R programming course
+swirl() #start swirl
